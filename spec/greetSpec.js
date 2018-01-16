6c9e597628c28f570446266f5cfbfaaa58ea245d
Alex <herdadvance@gmail.com>
2018-01-16 00:08:32 -0500

    Added bishop tests

--- spec/greetSpec.js
+++ spec/greetSpec.js
@@ -1,12 +1,60 @@
 'use strict';
 
-var greet = require('../src/greet');
+// var greet = require('../src/greet');
 
-describe('greet', function() {
-	it('should greet the given name', function() {
-		expect(greet('Alex')).toEqual('Hello Alex!');
+// describe('greet', function() {
+// 	it('should greet the given name', function() {
+// 		expect(greet('Alex')).toEqual('Hello Alex!');
+// 	});
+// 	it('should greet no-one special if no name is given', function() {
+// 		expect(greet()).toEqual('Hello world!');
+// 	});
+// });
+
+var knightMove = require('../src/greet');
+var bishopMove = require('../src/greet');
+
+describe('knight', function() {
+	it('should allow legal move', function() {
+		expect(knightMove(2,3)).toEqual(true);
 	});
-	it('should greet no-one special if no name is given', function() {
-		expect(greet()).toEqual('Hello world!');
+	it('should allow legal move', function() {
+		expect(knightMove(3,2)).toEqual(true);
+	});
+	it('should not allow an illegal move', function() {
+		expect(knightMove(2,2)).toEqual(false);
+	});
+	it('should not allow an illegal move', function() {
+		expect(knightMove(2,4)).toEqual(false);
+	});
+	it('should not allow an illegal move', function() {
+		expect(knightMove(4,1)).toEqual(false);
+	});
+});
+
+describe('bishop', function() {
+	it('should allow top and left diagonal', function() {
+		expect(bishopMove(2,5)).toEqual(true);
+	});
+	it('should allow top and right diagonal', function() {
+		expect(bishopMove(7,8)).toEqual(true);
+	});
+	it('should allow down and left diagonal', function() {
+		expect(bishopMove(1,6)).toEqual(true);
+	});
+	it('should allow down and right diagonal', function() {
+		expect(bishopMove(6,1)).toEqual(true);
+	});
+	it('should not allow horizontal', function() {
+		expect(bishopMove(3,7)).toEqual(false);
+	});
+	it('should not allow vertical', function() {
+		expect(bishopMove(6,4)).toEqual(false);
+	});
+	it('should not allow random', function() {
+		expect(bishopMove(5,8)).toEqual(false);
+	});
+	it('should not allow same space', function() {
+		expect(bishopMove(3,4)).toEqual(false);
 	});
 });
--- src/greet.js
+++ src/greet.js
@@ -8,4 +8,45 @@ var greet = function(name) {
 	return 'Hello ' + name + '!';
 };
 
-module.exports = greet;
+var knightMove = function(x,y) {
+	var currentX = 1;
+	var currentY = 1;
+
+	var xDifference = x - currentX;
+	var yDifference = y - currentY;
+
+	switch(xDifference){
+		case 1: 
+			if(yDifference == 2) return true;
+				else return false;
+			break;
+		case 2: 
+			if(yDifference == 1) return true;
+				else return false;
+			break;
+		default:
+			return false;
+			break;
+	}
+}
+
+var bishopMove = function(x,y){
+	var currentX = 3;
+	var currentY = 4;
+
+	var xDifference = x - currentX;
+	var yDifference = y - currentY;
+
+	var xAbs = Math.abs(xDifference);
+	var yAbs = Math.abs(yDifference);
+
+
+	if(3 == 3){
+		return true;
+	} else{
+		return false;
+	}
+}
+
+module.exports = knightMove, bishopMove;
+
