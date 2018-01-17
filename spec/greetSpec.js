340c59ab21e8b739631adb518e57df4e5f5dad28
Alex <herdadvance@gmail.com>
2018-01-16 22:52:23 -0500

    Added logic for horizontal, vertical, and king moves

--- spec/greetSpec.js
+++ spec/greetSpec.js
@@ -11,50 +11,145 @@
 // 	});
 // });
 
-var knightMove = require('../src/greet');
-var bishopMove = require('../src/greet');
+var vertical = require('../src/greet');
 
-describe('knight', function() {
-	it('should allow legal move', function() {
-		expect(knightMove(2,3)).toEqual(true);
-	});
-	it('should allow legal move', function() {
-		expect(knightMove(3,2)).toEqual(true);
-	});
-	it('should not allow an illegal move', function() {
-		expect(knightMove(2,2)).toEqual(false);
-	});
-	it('should not allow an illegal move', function() {
-		expect(knightMove(2,4)).toEqual(false);
-	});
-	it('should not allow an illegal move', function() {
-		expect(knightMove(4,1)).toEqual(false);
-	});
-});
+// describe('knightMove', function() {
+// 	var x1 = 3;
+// 	var y1 = 4;
 
-describe('bishop', function() {
-	it('should allow top and left diagonal', function() {
-		expect(bishopMove(2,5)).toEqual(true);
+// 	it('should allow L-shaped move up and left', function() {
+// 		expect(knightMove(x1,y1, 1,5)).toEqual(true);
+// 	});
+// 	it('should allow other L-shaped move up and left', function() {
+// 		expect(knightMove(x1,y1, 2,6)).toEqual(true);
+// 	});
+// 	it('should allow L-shaped move up and right', function() {
+// 		expect(knightMove(x1,y1, 5,5)).toEqual(true);
+// 	});
+// 	it('should allow other L-shaped move up and right', function() {
+// 		expect(knightMove(x1,y1, 4,6)).toEqual(true);
+// 	});
+// 	it('should allow L-shaped move down and left', function() {
+// 		expect(knightMove(x1,y1, 1,3)).toEqual(true);
+// 	});
+// 	it('should allow other L-shaped move down and left', function() {
+// 		expect(knightMove(x1,y1, 2,2)).toEqual(true);
+// 	});
+// 	it('should allow L-shaped move down and right', function() {
+// 		expect(knightMove(x1,y1, 5,3)).toEqual(true);
+// 	});
+// 	it('should allow other L-shaped move down and right', function() {
+// 		expect(knightMove(x1,y1, 4,2)).toEqual(true);
+// 	});
+// 	it('should not allow horizontal', function() {
+// 		expect(knightMove(x1,y1, 5,4)).toEqual(false);
+// 	});
+// 	it('should not allow vertical', function() {
+// 		expect(knightMove(x1,y1, 3,2)).toEqual(false);
+// 	});
+// 	it('should not allow diagonal', function() {
+// 		expect(knightMove(x1,y1, 7,8)).toEqual(false);
+// 	});
+// 	it('should not allow random', function() {
+// 		expect(knightMove(x1,y1, 5,1)).toEqual(false);
+// 	});
+// 	it('should not allow same space', function() {
+// 		expect(knightMove(x1,y1, 3,4)).toEqual(false);
+// 	});
+// });
+
+// describe('kingMove', function() {
+// 	var x1 = 3;
+// 	var y1 = 4;
+
+// 	it('should allow 1 square horizontal', function() {
+// 		expect(kingMove(x1,y1, 4,4)).toEqual(true);
+// 	});
+// 	it('should allow 1 square vertical', function() {
+// 		expect(kingMove(x1,y1, 3,3)).toEqual(true);
+// 	});
+// 	it('should allow 1 square diagonal ', function() {
+// 		expect(kingMove(x1,y1, 2,5)).toEqual(true);
+// 	});
+// 	it('should not allow 2 squares', function() {
+// 		expect(kingMove(x1,y1, 5,4)).toEqual(false);
+// 	});
+// 	it('should not allow random', function() {
+// 		expect(kingMove(x1,y1, 7,7)).toEqual(false);
+// 	});
+// 	it('should not allow same space', function() {
+// 		expect(kingMove(x1,y1, 3,4)).toEqual(false);
+// 	});
+// });
+
+// describe('diagonal', function() {
+// 	var x1 = 3;
+// 	var y1 = 4;
+
+// 	it('should allow top and left diagonal', function() {
+// 		expect(diagonal(x1,y1, 2,5)).toEqual(true);
+// 	});
+// 	it('should allow top and right diagonal', function() {
+// 		expect(diagonal(x1,y1, 7,8)).toEqual(true);
+// 	});
+// 	it('should allow down and left diagonal', function() {
+// 		expect(diagonal(x1,y1, 1,6)).toEqual(true);
+// 	});
+// 	it('should allow down and right diagonal', function() {
+// 		expect(diagonal(x1,y1, 6,1)).toEqual(true);
+// 	});
+// 	it('should not allow horizontal', function() {
+// 		expect(diagonal(x1,y1, 3,7)).toEqual(false);
+// 	});
+// 	it('should not allow vertical', function() {
+// 		expect(diagonal(x1,y1, 6,4)).toEqual(false);
+// 	});
+// 	it('should not allow random', function() {
+// 		expect(diagonal(x1,y1, 5,8)).toEqual(false);
+// 	});
+// 	it('should not allow same space', function() {
+// 		expect(diagonal(x1,y1, 3,4)).toEqual(false);
+// 	});
+// });
+
+// describe('horizontal', function() {
+// 	var x1 = 3;
+// 	var y1 = 4;
+
+// 	it('should allow moving right', function() {
+// 		expect(horizontal(x1,y1, 7,4)).toEqual(true);
+// 	});
+// 	it('should allow moving left', function() {
+// 		expect(horizontal(x1,y1, 1,4)).toEqual(true);
+// 	});
+// 	it('should not allow diagonal', function() {
+// 		expect(horizontal(x1,y1, 1,6)).toEqual(false);
+// 	});
+// 	it('should not allow random', function() {
+// 		expect(horizontal(x1,y1, 5,8)).toEqual(false);
+// 	});
+// 	it('should not allow same space', function() {
+// 		expect(horizontal(x1,y1, 3,4)).toEqual(false);
+// 	});
+// });
+
+describe('vertical', function() {
+	var x1 = 3;
+	var y1 = 4;
+
+	it('should allow moving up', function() {
+		expect(vertical(x1,y1, 3,8)).toEqual(true);
 	});
-	it('should allow top and right diagonal', function() {
-		expect(bishopMove(7,8)).toEqual(true);
+	it('should allow moving down', function() {
+		expect(vertical(x1,y1, 3,2)).toEqual(true);
 	});
-	it('should allow down and left diagonal', function() {
-		expect(bishopMove(1,6)).toEqual(true);
-	});
-	it('should allow down and right diagonal', function() {
-		expect(bishopMove(6,1)).toEqual(true);
-	});
-	it('should not allow horizontal', function() {
-		expect(bishopMove(3,7)).toEqual(false);
-	});
-	it('should not allow vertical', function() {
-		expect(bishopMove(6,4)).toEqual(false);
+	it('should not allow diagonal', function() {
+		expect(vertical(x1,y1, 7,8)).toEqual(false);
 	});
 	it('should not allow random', function() {
-		expect(bishopMove(5,8)).toEqual(false);
+		expect(vertical(x1,y1, 6,2)).toEqual(false);
 	});
 	it('should not allow same space', function() {
-		expect(bishopMove(3,4)).toEqual(false);
+		expect(vertical(x1,y1, 3,4)).toEqual(false);
 	});
 });
--- src/greet.js
+++ src/greet.js
@@ -8,45 +8,74 @@ var greet = function(name) {
 	return 'Hello ' + name + '!';
 };
 
-var knightMove = function(x,y) {
-	var currentX = 1;
-	var currentY = 1;
+var knightMove = function(x1,y1,x2,y2) {
 
-	var xDifference = x - currentX;
-	var yDifference = y - currentY;
+	var xAbs = Math.abs(x2 - x1);
+	var yAbs = Math.abs(y2 - y1);
 
-	switch(xDifference){
+	switch(xAbs){
 		case 1: 
-			if(yDifference == 2) return true;
+			if(yAbs == 2) return true;
 				else return false;
 			break;
 		case 2: 
-			if(yDifference == 1) return true;
+			if(yAbs == 1) return true;
 				else return false;
 			break;
 		default:
 			return false;
 			break;
 	}
+};
+
+var kingMove = function(x1,y1,x2,y2){
+
+	var xAbs = Math.abs(x2 - x1);
+	var yAbs = Math.abs(y2 - y1);
+
+	if(xAbs < 2 && yAbs < 2){
+		if(xAbs > 0 || yAbs > 0){
+			return true;
+		}
+	}
+	return false;
 }
 
-var bishopMove = function(x,y){
-	var currentX = 3;
-	var currentY = 4;
+var whitePawnMove = function(x1,y1,x2,y2){
 
-	var xDifference = x - currentX;
-	var yDifference = y - currentY;
+}
 
-	var xAbs = Math.abs(xDifference);
-	var yAbs = Math.abs(yDifference);
+var diagonal = function(x1,y1,x2,y2){
+
+	var xAbs = Math.abs(x2 - x1);
+	var yAbs = Math.abs(y2 - y1);
 
 
-	if(3 == 3){
+	if(xAbs == yAbs && x1 != x2){
 		return true;
 	} else{
 		return false;
 	}
-}
+};
 
-module.exports = knightMove, bishopMove;
+var horizontal = function(x1,y1,x2,y2){
 
+	if(x1 != x2 && y1 == y2){
+		return true;
+	} else{
+		return false;
+	}
+};
+
+var vertical = function(x1,y1,x2,y2){
+
+	if(x1 == x2 && y1 != y2){
+		return true;
+	} else{
+		return false;
+	}
+};
+
+
+//module.exports = knightMove, diagonal, horizontal, vertical;
+module.exports = vertical;
